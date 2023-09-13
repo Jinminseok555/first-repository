4f569a145a35009b5a212cad632a9e90dd1c3f8a
Jinminseok555 <jms89565388@gmail.com>
2023-09-13 15:23:56 +0900

    Create 1일차.py

---
+++ 1일차.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 14:38:11 2023
+
+@author: jms89
+"""
+a=6
+b=5
+print(a+b)
+
+school="금오공과대학교"
+student="진민석"
+print(school + student)
+
+print("오픈소스 열심히 공부하자!")
